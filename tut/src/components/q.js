12b8d3d73d3da62b161ae6de84b616541a857b97
Manu Ioan <ioanmanu@yahoo.com>
2021-11-11 10:10:16 +0200

    0.0.1

---
+++ tut/src/components/q.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function q() {
+    return (
+        <div className="qst">
+            Q1
+        </div>
+    )
+}
---
+++ tut/src/components/qs.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+
+const qs = () => {
+    return (
+        <div className="tab">
+           <p>Question</p>
+           <div className="chen">
+           
+           <div className="DOI">
+           <div className="qst">
+            Q1
+        </div>
+        <div className="qst">
+            Q2
+        </div>
+           </div>
+           
+           <div className="DOI">
+           <div className="qst">
+            Q3
+        </div>
+        <div className="qst">
+            Q4
+        </div>
+            </div>
+
+           </div>
+
+
+        </div>
+    )
+}
+
+
+
+export default qs
+
